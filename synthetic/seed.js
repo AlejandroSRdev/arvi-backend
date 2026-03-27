5cd1d9e5dabd9a4914114ee54c308105527e9708
AlejandroSRdev <06alejandrosr@gmail.com>
2026-03-27 15:44:53 +0100

    .env added to /synthetic

--- synthetic/seed.js
+++ synthetic/seed.js
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv'
+dotenv.config({ path: new URL('../.env', import.meta.url).pathname })
 dotenv.config({ path: new URL('.env.synthetic', import.meta.url).pathname })
 import admin from 'firebase-admin'
 import { scrypt, randomBytes, randomUUID } from 'node:crypto'
